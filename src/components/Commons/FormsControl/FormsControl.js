e179facb2453b6ae4fa3137826c9379c2d509e04
Your Name <you@example.com>
2019-12-02 17:24:22 +0300

    loginPage styling

--- src/components/Commons/FormsControl/FormsControl.js
+++ src/components/Commons/FormsControl/FormsControl.js
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from '../../Login/Login.module.css';
 
 export const Input = ({input, meta, ...props}) => {
-    return <div>
-    <input {...input} {...props} className = {styles.loginInput + ' ' + (meta.touched && meta.error && styles.error)} />
-    {meta.touched && meta.error && <span className = {styles.errorSpan}>{meta.error}</span>}
+    return <div className={styles.validate_input + ' ' + (meta.touched && meta.error && styles.error)} data-validate = {meta.error}>
+    <input {...input} {...props} className = {styles.loginInput} />
     </div> 
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,41 +1,48 @@
 import React from 'react';
-import {reduxForm, Field} from 'redux-form';
-import {Input} from '../Commons/FormsControl/FormsControl';
-import {required} from '../../utilits/validator';
+import { reduxForm, Field } from 'redux-form';
+import { Input } from '../Commons/FormsControl/FormsControl';
+import { required } from '../../utilits/validator';
 import { connect } from 'react-redux';
 import { login, logout } from '../../redux/loginReducer';
 import withAuthRedirect from '../../hoc/authRedirect';
+import classes from './Login.module.css';
+import img from '../../img/img-01.png'
 
 const Login = (props) => {
     const onSubmit = (formData) => {
         props.login(formData.login, formData.password, formData.rememberMe);
     }
-    return <div>
-        <h1>LOGIN</h1>
-        <LoginReduxForm onSubmit = {onSubmit} />
+    return <div className={classes.login_page}>
+        <div className={classes.login_form_container} >
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
+        <div className={classes.login_page_img}>
+            <img src={img} alt="some img"/>
+        </div>
     </div>
 }
 
-const LoginForm = (props) => { 
+const LoginForm = (props) => {
     return (
-        <form onSubmit = {props.handleSubmit} >
-            <div>
-               <Field placeholder = {'Login'} name = {'login'} component = {Input} validate = {required} />
-            </div>
-            <div>
-                <Field placeholder = {'Password'} type = {'password'} name = {'password'} component = {Input} validate = {required} />
-            </div>
-            <div>
-                <Field type= {'checkbox'} name = {'rememberMe'} component = {'input'}/> Remember me
-            </div>
-            <div>
+        <form onSubmit={props.handleSubmit} >
+            
+                <Field placeholder={'Login'} name={'login'} component={Input} validate={required} />
+            
+            
+                <Field placeholder={'Password'} type={'password'} name={'password'} component={Input} validate={required} />
+            
+            
+                <Field type={'checkbox'} name={'rememberMe'} component={'input'} /> Remember me
+            
+            
                 <button>LOGIN</button>
-                {props.error && <div> {props.error} </div>}
-            </div>
+                {props.error && <div className={classes.incorrect_data}> {props.error} </div>}
+            
         </form>
     )
 }
 
-const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
-export default connect(null, {login, logout})(withAuthRedirect (Login));
+export default connect(null, { login, logout })(withAuthRedirect(Login));
